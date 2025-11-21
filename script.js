9e87a18fc6b54559564476f5bd3a3e0c1abe5cf5
ranjvee <146024023+ranjvee@users.noreply.github.com>
2025-11-21 18:19:28 +0200

    Add files via upload
    Source files for Calculator app

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-let count = 0;
-let countEl = document.getElementById('count-el');
-let saveEl = document.getElementById('save-el');
-console.log (countEl);
-function increment() {
-  count += 1;
-  countEl.innerText=count ;
-  console.log(count);
+let num1 = 6 ;
+let num2 = 3 ;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+function add() {
+  let sum = num1+ num2;
+  console.log(sum);
+  document.getElementById("sum-el").textContent = "Sum: " + sum;
 }
-document.getElementById('increment-btn').addEventListener('click', increment);
-function save() {
- let countStr = count + "-" ;
-  saveEl.textContent += countStr ;
-  countEl.textContent = 0
-  count = 0
+document.getElementById("add-btn").addEventListener("click",add);
+function sub(){
+  let sub = num1-num2;
+  document.getElementById("sum-el").textContent = "Sum: " + sub;
 }
-document.getElementById('save-btn').addEventListener('click', save);
+document.getElementById("subtract-btn").addEventListener("click",sub);
+function mul(){
+  let mul = num1 * num2;
+  document.getElementById("sum-el").textContent = "Sum: " + mul;
+}
+document.getElementById("multiply-btn").addEventListener("click",mul);
+function div(){
+  let div = num1 / num2;
+  document.getElementById("sum-el").textContent = "Sum: " + div;
+}
+document.getElementById("divide-btn").addEventListener("click",div);
